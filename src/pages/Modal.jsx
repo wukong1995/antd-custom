36abc53505588a249310d03373fa81400cd19f1b
Peipei Wong <wangpp1995@gmail.com>
2019-06-10 15:26:07 +0800

    Upgrade antd-custom version

--- src/pages/Modal.jsx
+++ src/pages/Modal.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
 
-import { Modal } from 'antd-custom'
+import { Modal, Tooltip } from 'antd-custom'
 
 export default function PageModal() {
   const [visible, setVisible] = useState(false);
 
   return (
     <div>
+      <Tooltip title="这是一个ToolTip">hover打开Tooltip</Tooltip>
       <button type="button" onClick={() => setVisible(true)}>点击打开modal</button>
       <Modal
         visible={visible}
